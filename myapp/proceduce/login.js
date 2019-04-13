9ac646a8d280e331293f20eeec548925672882f0
irish-luong <duclmq@ahamove.com>
2019-04-13 10:00:28 +0700

    deploy.app

--- myapp/proceduce/login.js
+++ myapp/proceduce/login.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
+const ObjectId = require('mongodb').ObjectID;
 const fs = require('fs');
 
-// Get mongo connection string 
+// Get mongo connection string
 const rawData = fs.readFileSync('./proceduce/serect_key.json');
 const rawData_toJSON = JSON.parse(rawData);
-const mongoConnString = rawData_toJSON["mongoString"];
+const mongoConnString = rawData_toJSON['mongoString'];
 
 mongoose.connect(mongoConnString, {
-    useNewUrlParser: true
+	useNewUrlParser: true,
 });
 
 const conn = mongoose.connection;
@@ -15,25 +16,89 @@ const conn = mongoose.connection;
 const User = conn.collection('users');
 
 var signUp = (email, password, callback) => {
-    let emailFinding = User.findOne({
-        "email": email
-    }, (error, result) => {
-        if (error) {
-            return callback('EFINDONE', error);
-        }
-        if (result) {
-            return callback(null, "account_exist");
-        }
-        User.insertOne({
-            "email": email,
-            "password": password,
-        }, (error, result) => {
-            if (error) {
-                return callback('EADD', error);
-            }
-            callback(null, "update success")
-        })
-    })
+	let emailFinding = User.findOne(
+		{ 
+			email: email,
+		},
+		(error, result) => {
+			if (error) {
+				return callback('EFINDONE', error);
+			}
+			if (result) {
+				return callback(null, 'account_exist');
+			}
+			User.insertOne(
+				{
+					email: email,
+					password: password,
+				},
+				(error, result) => {
+					if (error) {
+						return callback('EADD', error);
+					}
+					callback(null, 'update success');
+				}
+			);
+		}
+	);
+};
+
+var signIn = (email, password, callback) => {
+	let accountVerify = User.findOne(
+		{
+			email: email,
+			password: password,
+		},
+		(error, result) => {
+			if (error) {
+				return callback(1, error, false);
+			}
+			if (result) {
+				User.updateOne(
+					{
+						_id: result['_id'],
+					},
+					{
+						$set: {
+							status: 'ONLINE',
+						},
+					},
+					() => {
+						return callback(0, 'Sign in successfully', true, result);
+					}
+				);
+			} else {
+				return callback(3, 'Account is not exist', false);
+			}
+		}
+	);
+};
+
+var signOut = (_id, callback) => {
+	User.findOne({ "_id": ObjectId(_id)}, {"_id": 1}, (error, result) => {
+        if(result && result["status"] === 'ONLINE'){
+		User.updateOne(
+			{
+				_id: ObjectId(_id),
+			},
+			{
+				$set: {
+					status: 'OFFLINE',
+				},
+			},
+			(err, result) => {
+				if (err) {
+					return callback(1, err, false);
+				} else {
+					return callback(0, 'Signout successfully', true);
+				}
+			}
+        )} else if (result && result["status"] != 'ONLINE') {
+            return callback(3, "Status is not Online", false);
+        } else{
+            return callback(3, "Object is not found", false)
+        };
+	});
 };
 
 // DEBUG function
@@ -41,4 +106,25 @@ var signUp = (email, password, callback) => {
 //     console.log('result= ${result}');
 // });
 
-module.exports = signUp;
+// signIn('duclmq_3@gmail.com', 'hello', (errorCode, message, signin, userInfo) => {
+//     console.log({
+//         "errorCode": errorCode,
+//         "message": message,
+//         "signin": signin,
+//         "userInfo": userInfo,
+//     })
+// })
+
+// signOut('5caeea066bfcdd351c9a566f', (errorCode, message, signout) => {
+// 	console.log({
+// 		errorCode: errorCode,
+// 		message: message,
+// 		signout: signout,
+// 	});
+// });
+
+module.exports = {
+	signIn,
+	signUp,
+	signOut,
+};
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,22 +1,45 @@
 const express = require('express');
 const router = express.Router();
 
-const signUp = require('../proceduce/login.js');
+const { signIn, signUp, signOut } = require('../proceduce/login.js');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res, next) => {
+	res.send('respond with a resource');
 });
 
-router.get('/signup/:email/:password', function (req, res) {
-  let email = req.params.email;
-  let password = req.params.password;
-  signUp(email, password, (err, result) => {
-    res.send(result);
-  });
+/* GET check user and ->> create new account. */
+router.get('/signup/:email/:password', (req, res) => {
+	let email = req.params.email;
+	let password = req.params.password;
+	signUp(email, password, (err, result) => {
+		res.send(result);
+	});
 });
 
+/*GET check user and login */
+router.get('/signin/:email/:password', (req, res) => {
+	let email = req.params.email;
+	let password = req.params.password;
+	signIn(email, password, (errorCode, message, signin, userInfo) => {
+		res.send({
+			errorCode: errorCode,
+			message: message,
+			signin: signin,
+			userInfo: userInfo,
+		});
+	});
+});
 
-
+router.get('/signout/:_id', (req, res) => {
+	let _id = req.params._id;
+	signOut(_id, (errorCode, message, signout) => {
+		res.send({
+			errorCode: errorCode,
+			message: message,
+			signout: signout,
+		});
+	});
+});
 
 module.exports = router;
