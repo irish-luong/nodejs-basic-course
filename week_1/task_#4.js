2f375961b36e4371069d440df3246321354d8b51
irish-luong <duclmq@ahamove.com>
2019-04-16 17:52:17 +0700

    push_week1

--- week_1/task_#4.js
+++ week_1/task_#4.js
@@ -1,7 +1,34 @@
-const nameList = ['Nam', 'Mai', 'Trang', 'An', 'Thien'];
+const students = [{
+    "name": 'Nam',
+    "age": 24,
+    "gender": 'male',
+},
+{
+    "name": 'Mai',
+    "age": 22,
+    "gender": 'female',
+},
+{
+    "name": 'Trang',
+    "age": 23,
+    "gender": 'female',
+},
+{
+    "name": 'An',
+    "age": 20,
+    "gender": 'male',
+},
+{
+    "name": 'Thien',
+    "age": 27,
+    "gender": 'male',
+},
+];
 
-var namelistConver = nameList.map((x) => {
-    return x
+
+
+var namelistConver = students.map((x) => {
+    return x["name"]
 });
 
 console.log(namelistConver)
